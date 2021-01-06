88c4a36fcdc29871a6afd0f25b2a297ea48cde9a
Jonah <jonahsilverman11@gmail.com>
2021-01-06 16:41:23 -0500

    Added Clickable Markers, displaying popups

--- lib/MapPage.dart
+++ lib/MapPage.dart
@@ -12,19 +12,89 @@ class _MapPageState extends State<MapPage> {
   CameraPosition _center = CameraPosition(target: LatLng(-45.71, 75));
   Location location = new Location();
   GoogleMapController mapController;
-  double currentZoom = 15.0;
+  //double currentZoom = 15.0;
+  bool tracking = true;
 
   double currentSliderVal = 15.0;
+  
+  final Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  
+  // MarkerId markerId1 = new MarkerId("1");
+  Marker marker1 = new Marker(
+    markerId: MarkerId("1"),
+    position: LatLng(40.5975, -75.5),
+    visible: true,
+    draggable: false,
+    );
+
+  
 
   //Function called when GoogleMap is implemented which, upon consent, tracks the user
   void onMapCreated(GoogleMapController controller) {
     mapController = controller;
-    location.onLocationChanged.listen((event) {
-      mapController.animateCamera(CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(event.latitude, event.longitude), zoom: currentZoom )
-      ));
+
+      // location.onLocationChanged.listen((event) {
+      //   mapController.animateCamera( CameraUpdate.newCameraPosition(
+      //       CameraPosition(
+      //         target: LatLng(event.latitude, event.longitude),
+      //
+      //       )
+      //   ));
+      // }
+    //  );
     }
-    );
+
+    Marker addMarkerFunc(Marker marker)
+    {
+      marker = new Marker(
+          markerId: MarkerId("1"),
+          position: LatLng(40.5975, -75.5),
+          visible: true,
+          draggable: false,
+          onTap: () {
+            setState(() {
+              createAlertDialog(context, "Ritas");
+              print("Hello World");
+            });
+          });
+      return marker;
+
+    }
+
+
+    createAlertDialog(BuildContext context, String name){
+        return showDialog(context: context, builder: (context) {
+          return AlertDialog(
+            title: Text(name),
+            content: Column(
+              children: [
+                Icon(Icons.accessible_forward)
+              ],
+            ),
+          );
+        } );
+    }
+
+
+
+  // Future<bool> changeSettings({
+  //   LocationAccuracy accuracy = LocationAccuracy.high,
+  //   int interval = 1000,
+  //   double distanceFilter = 0,
+  // }) {
+  //   return LocationPlatform.instance.changeSettings(
+  //     accuracy: accuracy,
+  //     interval: interval,
+  //     distanceFilter: distanceFilter,
+  //   );
+  // }
+
+
+  @override
+  void initState() {
+    marker1 = addMarkerFunc(marker1);
+    markers[marker1.markerId] = marker1;
+    super.initState();
   }
 
   @override
@@ -37,6 +107,7 @@ class _MapPageState extends State<MapPage> {
           mapType: MapType.hybrid,
           myLocationEnabled: true,
           myLocationButtonEnabled: true,
+          markers: Set<Marker>.of(markers.values),
         ),
             Positioned(
               bottom: 10.0,
@@ -49,9 +120,10 @@ class _MapPageState extends State<MapPage> {
                   onChanged: (double val) {
                     setState(() {
                       currentSliderVal = val;
-                      currentZoom = val /2;
+                      //currentZoom = val /2;
                   });
-                }))
+                })),
+
 
       ],
     );
